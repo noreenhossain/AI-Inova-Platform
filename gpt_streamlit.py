3cda7781bbed0fe07a2497c4e265d303a5d3bba2
noreenhossain <139469631+noreenhossain@users.noreply.github.com>
2023-10-10 10:46:57 -0400

    Streamlit v1 (#1)
    * Working ish streamlit prototype
    
    * Updated streamlit page

--- gpt_streamlit.py
+++ gpt_streamlit.py
@@ -1,48 +1,124 @@
 # Importing necessary libraries
 import streamlit as st
 import openai
+from PyPDF2 import PdfReader
+import pyperclip
 
-openai.api_key = "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"
+import docx2txt
+
+
+# broadcasting and set host for streamlit app 
 
 st.set_page_config(layout="wide")
 
 # Main page with 3 rows and 2 columns
-st.title("Claude LLM Parsing")
+st.title("GPT Deck Summarizer")
 
-with st.expander("Prompt", expanded=True):
-    prompt_text = st.text_area("Prompt", value="Summarize this text: ")
+# OpenAI API Key
+# openai.api_key = ""
 
-col1_1, col1_2 = st.columns([3, 2])
+# Method to process uploaded PDFs
+def read_pdf(file):
+	pdfReader = PdfReader(file)
+	count = len(pdfReader.pages)
+	all_page_text = ""
+	for i in range(count):
+		page = pdfReader.pages[i]
+		all_page_text += page.extract_text()
 
+	return all_page_text
+
+# Default prompt text that can be modified in Streamlit window
+prompt_text = """Please provide a profile of the company described in this text, including if it is public or private,​ who the founders are, its founding date, and any parent companies if possible. Next, name the key asset described in the text in the format "Name of Asset (Name of Company)". Next, describe the key asset in two paragraphs, including key information about the stage it is in, and any future milestones mentioned. Next, describe how it compares to similar assets that exist. Next, repeat description for all other assets mentioned in the text. Finally, print "Summary Complete" """
+gpt_model = "gpt-4"
+
+
+# Setting up the document
+document_text = ""
+
+# Add a file uploader
+docx_file = st.file_uploader("Upload File",type=['txt','docx', 'pdf'])
+
+# Checks the uploaded file and stores information about it
+if docx_file is not None:
+    file_details = {"Filename":docx_file.name,"FileType":docx_file.type,"FileSize":docx_file.size}
+    # Confirmation that file was uploaded successfully if file details written
+    st.write(file_details)
+    # Check File Type
+    # TXT
+    if docx_file.type == "text/plain":
+        st.text(str(docx_file.read(),"utf-8")) # empty
+        document_text = str(docx_file.read(),"utf-8") # works with st.text and st.write,used for futher processing
+    # PDF
+    elif docx_file.type == "application/pdf":
+        document_text = read_pdf(docx_file) 
+    # DOCX
+    elif docx_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+    # Use the right file processor ( Docx,Docx2Text,etc)
+        document_text = docx2txt.process(docx_file) # Parse in the uploadFile Class directory
+        # st.text(type(document_text))
+
+# Make Columns for Propmpt box and summary box
+col1_1, col1_2 = st.columns([2, 3])
+
+# Prompt Box
 with col1_1:
-    indications_text = st.text_area(
-        "Text", 
-        "",
-        key="text",
-        height=220)
-    
+    with st.expander("Prompt", expanded=True):
+            prompt_box = st.text_area("Here is the prompt to summarize a deck. Feel free to adjust the prompt if needed.", height = 300, key = "prompt_box", value=prompt_text)
+
+    def reset_prompt():
+        st.session_state.prompt_box = prompt_text
+
+    st.button("Reset Prompt Text", on_click = reset_prompt)
+
+# Generated Summary Box
 with col1_2:
     output = ""
-    indications_output = st.text_area("Summary",
+    a = st.text_area("GPT Summary of Deck",
         output,
-        height=220,
+        height=500,
         key="output_text",
-        placeholder="Click 'GPT Summary' to update")
+        placeholder="Please click \'Get GPT Summary\' to generate summary")
 
+    # Function to generate GPT Response using prompt and document
     def on_gpt_indication_click():
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=[{"role": "user", "content": f"{prompt_text}: {indications_text}"}],
+            model= gpt_model,
+            messages=[{"role": "user", "content": f"{prompt_box}: {document_text}"}],
             temperature=0,
-            max_tokens=500,
+            # max_tokens=500,
             top_p=1.0,
             frequency_penalty=0.0,
             presence_penalty=0.0
         )
+        # return response['choices'][0]['message']['content']
+        st.session_state.output_text = response['choices'][0]['message']['content']
 
-        output = response['choices'][0]['message']['content']
+    # Function to clear text box
+    def clear_box():
+        st.session_state.output_text = ""
 
-        # Way to access Streamlit settion's "output_text" key
-        st.session_state.output_text = output
+    # Make action buttons
+    summary, clear, copy = st.columns([1,1,1])
 
-    st.button("Get GPT Summary", key="get_gpt_button", on_click=on_gpt_indication_click)
+
+    with summary:
+        st.button("Get GPT Summary", key="get_gpt_button", on_click=on_gpt_indication_click)
+        # if st.button("Get GPT Summary"):
+            # st.session_state.output_text = on_gpt_indication_click()
+    with clear:
+        if st.button("Clear Output", on_click = clear_box):
+            del st.session_state.output_text
+            st.session_state.output_text = "Blank"
+    with copy:
+        if st.button('Copy Summary Text'):
+            pyperclip.copy(a)
+            st.success('Text copied successfully!')
+
+
+    
+
+
+    
+
+    
